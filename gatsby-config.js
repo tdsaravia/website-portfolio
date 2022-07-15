2938c0a706ba1c0331671b4ace2b9d8b8535f501
tobiisaravia <tttobis@gmail.com>
2022-07-14 22:45:41 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
