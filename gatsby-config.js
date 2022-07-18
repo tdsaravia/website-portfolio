737221f228a432f6a1dab00ef4d07a4f0fcf94f6
tobiisaravia <tttobis@gmail.com>
2022-07-18 08:02:15 -0300

    gh pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
+  pathPrefix: "/website-portfolio",
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
-    pathPrefix: "/website-portfolio",
-
   },
   plugins: [ "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
